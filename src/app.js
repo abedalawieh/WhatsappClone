e5deee92659f17597e1c7187581f33592b8f88e6
abedalawieh <abedalaweye@gmail.com>
2024-02-01 22:55:08 +0200

    added the middlewares

--- src/app.js
+++ src/app.js
@@ -1,12 +1,40 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+import fileUpload from "express-fileupload";
 
 //DOTENV config
 dotenv.config();
+
 //Create Express App
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello");
+//morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+//helmet
+app.use(helmet());
+//parse json body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//sanitize request data
+app.use(mongoSanitize());
+//enable cookie parser
+app.use(cookieParser());
+//gzip compression
+app.use(compression());
+//file upload
+app.use(fileUpload({ useTempFiles: true }));
+//cors
+app.use(cors({ origin: "http://localhost:3000" }));
+app.post("/test", (req, res) => {
+  res.send(req.body);
 });
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import app from "./app.js";
-import dotenv from "dotenv";
-
-//DOTENV config
-dotenv.config();
 
 //env variables
 const PORT = process.env.PORT;
-
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
