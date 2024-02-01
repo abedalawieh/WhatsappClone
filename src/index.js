e8d5bf61f41c392fd7167acbf88c7bdbe460ee2e
abedalawieh <abedalaweye@gmail.com>
2024-02-01 23:09:49 +0200

    Custom logger ,added logger.config.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
-
+import logger from "./config/logger.config.js";
 //env variables
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  logger.info(`Server Listening on port ${PORT}`);
 });
